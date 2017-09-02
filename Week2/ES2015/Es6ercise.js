4a23272b4aac17cf1664400523bc568d9a23e9a6
coej94 <coej94@hotmail.com>
2017-09-02 18:21:15 +0200

    exercise 6 completed

--- Week2/ES2015/Es6ercise.js
+++ Week2/ES2015/Es6ercise.js
@@ -111,4 +111,28 @@ console.log("b)");
 var rest = [true,2,"hello World",[1,2,3],new Date(),{}]; 
 var restParams = [ ...rest]; 
 console.log(f(5,2,...restParams));
+/*
+console.log("c)");
+var chars = [... f(5,2,...restParams)];
+console.log('------------------------------------');
+console.log(chars); //Why does this throw an error?
+console.log('------------------------------------');
+*/
+//Exercise 6: Shorthand notation
+console.log("exercise 6:");
+//Initializing
+let fName = "Kurt";
+let lName = "Wonnegut";
+let age = 98
 
+//this is easier to write instead of "this.fname = fname etc.."
+let man = {
+    fName: fName,
+    lName: lName,
+    age: age
+};
+
+//This line shows us the two ways we can acces the variables of our object.
+console.log('------------------------------------');
+console.log(`Mr. ${man['fName']} ${man.lName} is ${man.age} years old!`);  
+console.log('------------------------------------');
