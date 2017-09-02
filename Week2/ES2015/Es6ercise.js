58cbe15b739440644ba532d273be39e67bc15c97
coej94 <coej94@hotmail.com>
2017-09-02 17:44:56 +0200

    exercise 5 completed

--- Week2/ES2015/Es6ercise.js
+++ Week2/ES2015/Es6ercise.js
@@ -95,3 +95,20 @@ a total of ${card.amount * card.unitprice} bucks?`
 console.log('------------------------------------');
 console.log(message);
 console.log('------------------------------------');
+
+//Exercise 5 Rest Parameters and Spread Operators
+
+console.log(`Exercise 5
+a)`);
+function f(x,y,...rest){ 
+    console.log(`sum: ${rest.length}`);
+    rest.forEach((item, index) => console.log(`rest value ${index+1} is a ${item.constructor.name}`));
+}
+console.log(f(5,2,true,2,"hello World",[1,2,3],new Date(),{}));
+
+console.log("b)");
+//To test it works!
+var rest = [true,2,"hello World",[1,2,3],new Date(),{}]; 
+var restParams = [ ...rest]; 
+console.log(f(5,2,...restParams));
+
