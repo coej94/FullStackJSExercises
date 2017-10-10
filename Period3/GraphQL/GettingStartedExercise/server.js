87167f1c12a7865201cabf60b8218c22dd63ae28
coej94 <coej94@hotmail.com>
2017-10-10 22:50:39 +0200

    more added to graphQL

--- Period3/GraphQL/GettingStartedExercise/server.js
+++ Period3/GraphQL/GettingStartedExercise/server.js
@@ -8,6 +8,8 @@ var schema = buildSchema(`
     quoteOfTheDay: String
     random: Float!
     rollThreeDice: [Int]
+    rollDice(numDice: Int!, numSides: Int!): [Int]
+    something: String!
   }
 `);
 
@@ -22,6 +24,13 @@ var root = {
   rollThreeDice: () => {
     return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
   },
+  rollDice: ({numDice, numSides}) => {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  }
 };
 
 var app = express();
