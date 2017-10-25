bdc23d5fa1f693ce615e6c9310c3deaa3573eee3
coej94 <coej94@hotmail.com>
2017-10-25 10:22:20 +0200

    components seperated in demo project

---
+++ Period4/demoapp/src/components/bananaComponent.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+
+const Banana = ({text}) => {
+    
+    let image = {
+        uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+      }
+      return(
+        <View style={styles.bananaStyle}>
+          <Image source={image} style={{width: 193, height: 110}}/>
+          <Text style={styles.textStyle}>{text}</Text>
+        </View>
+      );
+    };
+    
+    const styles = {
+      bananaStyle: {
+        marginTop: 30
+      }, 
+      textStyle: {
+      fontSize: 50
+    }
+}
+
+export default Banana;
